88ec1a75fc467c19890b653eca91a1dfba82b876
boitenge <jeromeboitenge@gmail.com>
2026-01-28 14:51:38 +0200

    mofresh-orders

--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -1,4 +1,22 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../../database/database.module';
+import { OrdersService } from './orders.service';
+import { OrdersController } from './orders.controller';
+// AIMEE: Uncomment when StockMovementsModule is ready
+// import { StockMovementsModule } from '../stock-movements/stock-movements.module';
+// STEVEN: Uncomment when InvoicesModule is ready
+// import { InvoicesModule } from '../invoices/invoices.module';
 
-@Module({})
-export class OrdersModule {}
+@Module({
+  imports: [
+    DatabaseModule,
+    // AIMEE: Uncomment when ready
+    // StockMovementsModule,
+    // STEVEN: Uncomment when ready
+    // InvoicesModule,
+  ],
+  controllers: [OrdersController],
+  providers: [OrdersService],
+  exports: [OrdersService],
+})
+export class OrdersModule { }
