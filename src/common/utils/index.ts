8681acf1120211f807c15d422b1f8c72c40d70e9
Jerome NZARAMYIMANA <138975785+jeromeboitenge@users.noreply.github.com>
2026-01-30 08:07:50 +0200

    Revert "Feature/orders"

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -1,3 +1,1 @@
 export * from './hashing.util';
-export * from './paginator'
-
--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -1,18 +1,4 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from '../../database/database.module';
-import { OrdersService } from './orders.service';
-import { OrdersController } from './orders.controller';
-// import { StockMovementsModule } from '../stock-movements/stock-movements.module';
-// import { InvoicesModule } from '../invoices/invoices.module';
 
-@Module({
-  imports: [
-    DatabaseModule,
-    // StockMovementsModule,
-    // InvoicesModule,
-  ],
-  controllers: [OrdersController],
-  providers: [OrdersService],
-  exports: [OrdersService],
-})
-export class OrdersModule { }
+@Module({})
+export class OrdersModule {}
