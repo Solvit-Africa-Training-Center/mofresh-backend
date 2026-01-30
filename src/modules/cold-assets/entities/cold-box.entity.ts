a47b5bd79ffb56b8fcd276d4bb7fcb4d304f0b6d
umu1aimee <umu1aimee@gmail.com>
2026-01-30 16:36:35 +0200

    feat: Added coldbox entity

---
+++ src/modules/cold-assets/entities/cold-box.entity.ts
@@ -0,0 +1,34 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { AssetStatus } from '@prisma/client';
+
+export class ColdBoxEntity {
+
+  @ApiProperty() 
+  id: string;
+
+  @ApiProperty({ example: 'CB-001' }) 
+  identificationNumber: string;
+
+  @ApiProperty() 
+  sizeOrCapacity: string;
+
+  @ApiProperty() 
+  siteId: string;
+
+  @ApiProperty() 
+  location: string;
+
+  @ApiProperty({ enum: AssetStatus }) 
+  status: AssetStatus;
+
+  @ApiProperty()
+   createdAt: Date;
+
+  @ApiProperty() 
+  updatedAt: Date;
+
+  @ApiProperty({ nullable: true }) 
+  deletedAt: Date | null;
+
+  constructor(partial: Partial<ColdBoxEntity>) { Object.assign(this, partial); }
+}
